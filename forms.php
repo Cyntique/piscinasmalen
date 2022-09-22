499d3708caf961e07e2c75bec88b340f432bfed2
Cyntique <cyntydev@gmail.com>
2022-09-21 23:48:48 -0300

    Correcciones en form

--- forms.php
+++ forms.php
@@ -3,25 +3,27 @@
 // Llamando a los campos
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $telefono  = $_POST['telefono'];
+    $telefono  = $_POST['phone'];
     $ciudad  = $_POST['ciudad'];
     $servicios = $_POST['servicios'];
+    $mensaje = $_POST['mensaje'];
     
 
 
 // Datos para el correo
-$destinatario = "cyntybod@gmail.com";
+$destinatario = "info@piscinasmalen.com.ar";
 $asunto = "Consulta de la web Piscinas Malen";
-$encabezados = "Enviado: cyntybod@gmail.com\r\n"; 
+$encabezados = "Enviado: info@piscinasmalen.com.ar\r\n"; 
 
-$carta = "De: $name \n;
+$carta = "De: $name \n";
 $carta .= "Email: $email \n";
 $carta .= "Telefono: $telefono \n";
 $carta .= "Ciudad: $ciudad \n";
 $carta .= "Servicios: $servicios \n";
+$carta .= "Mensaje: $mensaje \n";
 
 // Enviando Mensaje
 mail($destinatario, $asunto, $carta, $encabezados);
-header('Location:/envio.html');
+header('Location:envio.html');
 
 ?>
